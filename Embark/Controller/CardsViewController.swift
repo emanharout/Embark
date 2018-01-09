e96ad640d42e31c568370c6f288feceff84f35f6
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2018-01-08 16:52:09 -0800

    Create CardsViewController

---
+++ Embark/Controller/CardsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CardsViewController.swift
+//  Embark
+//
+//  Created by Emmanuoel Haroutunian on 1/8/18.
+//  Copyright © 2018 Emmanuoel Haroutunian. All rights reserved.
+//
+
+import UIKit
+
+class CardsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
