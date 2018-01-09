ab6bf44d59880c33a8dbe22d8aab578be79c4963
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2018-01-09 01:25:59 -0800

    Create CategoryCell

---
+++ Embark/View/CategoryCell.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryCell.swift
+//  Embark
+//
+//  Created by Emmanuoel Haroutunian on 1/9/18.
+//  Copyright © 2018 Emmanuoel Haroutunian. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+	
+	override func setSelected(_ selected: Bool, animated: Bool) {
+		super.setSelected(selected, animated: animated)
+		
+		// Configure the view for the selected state
+	}
+	
+}
