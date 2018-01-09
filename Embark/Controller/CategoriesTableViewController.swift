8f6c37ca7cbd6a3100ff06799dbf912e1425ce0c
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2018-01-09 01:25:52 -0800

    Create CategoriesTableViewController

---
+++ Embark/Controller/CategoriesTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CategoriesTableViewController.swift
+//  Embark
+//
+//  Created by Emmanuoel Haroutunian on 1/9/18.
+//  Copyright © 2018 Emmanuoel Haroutunian. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+			
+    }
+
+}
+
+extension CategoriesTableViewController {
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+	}
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 5
+	}
+}
