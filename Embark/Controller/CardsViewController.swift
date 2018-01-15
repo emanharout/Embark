fdb267cbfe051e7e42ab618d84477843e41b151c
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2018-01-15 00:46:08 -0800

    Create Cards View Controller

--- Embark/Controller/CardsViewController.swift
+++ Embark/Controller/CardsViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import Koloda
 
 class CardsViewController: UIViewController {
+	
+	var cardsView: CardsView { return view as! CardsView }
+	
+	let dummyCards = [
+		Question(text: "First Question", bonus: nil),
+		Question(text: "Second Question", bonus: nil),
+		Question(text: "Third Question", bonus: nil),
+		Question(text: "Fourth Question", bonus: nil),
+		Question(text: "Fifth Question", bonus: nil)
+	]
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		cardsView.kolodaView.delegate = self
+		cardsView.kolodaView.dataSource = self
+	}
+	
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-
+extension CardsViewController: KolodaViewDelegate, KolodaViewDataSource {
+	func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+		let view = QuestionCardView(frame: CGRect.zero)
+		view.model = QuestionCardView.Model(question: dummyCards[index], index: index)
+		return view
+	}
+	
+	func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+		return dummyCards.count
+	}
+	
+	func kolodaSpeedThatCardShouldDrag(_ koloda: KolodaView) -> DragSpeed {
+		return DragSpeed.default
+	}
 }
