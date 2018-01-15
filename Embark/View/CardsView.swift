a55b181f3d57364b7bd72e59facdbea4cbc3b2e3
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2018-01-15 00:45:39 -0800

    Create Card View
    Koloda requires a view to be return from its delegates to display as the card.

--- Embark/View/CardsView.swift
+++ Embark/View/CardsView.swift
@@ -7,15 +7,10 @@
 //
 
 import UIKit
+import Koloda
 
 class CardsView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+	
+	@IBOutlet var kolodaView: KolodaView!
+	
 }
